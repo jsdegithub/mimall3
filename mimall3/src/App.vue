d6d7db97afc3ea356064489bb74fd315fc75e4b6
jsdegithub <def_myself@qq.com>
2021-03-08 15:40:00 +0800

    完成NavHeader组件

--- mimall3/src/App.vue
+++ mimall3/src/App.vue
@@ -1,12 +1,33 @@
 <template>
     <div id="app">
-        APP.vue
         <router-view></router-view>
     </div>
 </template>
 
+<script>
+    export default {
+        data() {
+            return {};
+        },
+        mounted() {
+            console.log("------------------------------");
+            console.log("------------------------------");
+            console.log(
+                "该页面的window.location.hash值为：" + window.location.hash
+            );
+            console.log("------------------------------");
+            console.log("------------------------------");
+            console.log(
+                "该页面的window.location.href值为：" + window.location.href
+            );
+            console.log("------------------------------");
+            console.log("------------------------------");
+            console.log(
+                "该页面的this.$router值为：" + this.$router
+            );
+        },
+    };
+</script>
+
 <style lang="scss">
-    #app {
-        font-size: 30px;
-    }
 </style>
--- mimall3/src/main.js
+++ mimall3/src/main.js
@@ -1,28 +1,46 @@
 import Vue from "vue";
 import axios from "axios";
+import VueLazyLoad from "vue-lazyload";
 
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import env from "./env";
+// import env from "./env";
 
 Vue.config.productionTip = false;
 Vue.prototype.axios = axios;
 
 axios.defaults.baseURL = "/api";
 // 根据开发环境获取不同的 api 接口域名
-debugger
-axios.defaults.baseURL = env.baseURL;
+// debugger
+// axios.defaults.baseURL = env.baseURL;
 axios.defaults.timeout = 8000;
-axios.interceptors.response.use(function(res) {
-    var data = res.data;
-    if (res.status === 0) {
-        return data;
-    } else if (res.status === 10) {
-        window.location.href = "/#/login";
-    } else {
-        alert(res.msg);
+axios.interceptors.response.use(
+    function(response) {
+        let res = response.data;
+        let path = location.hash;
+        if (res.status == 0) {
+            return res.data;
+        } else if (res.status == 10) {
+            // 如果在主页就不让它跳转，让用户自己点击登录按钮登录
+            if (path != "#/index") {
+                window.location.href = "/#/login";
+            }
+            return Promise.reject();
+        } else {
+            return Promise.reject(res);
+        }
+    },
+    (error) => {
+        let res = error.response;
+        if (res.data.status == 500) {
+            return Promise.reject();
+        }
     }
+);
+
+Vue.use(VueLazyLoad, {
+    loading: "/imgs/loading-svg/loading-bars.svg",
 });
 
 new Vue({
